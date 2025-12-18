81d026e137eeb5b9f4403214bd8e6a8da8903b45
Even Berge Sandvik <evensandvik207@gmail.com>
2025-12-18 12:39:40 +0100

    Update generate-version.sh
    Co-authored-by: Johannes Bråthen Oma <johannes.oma@kartverket.no>

--- generate-version.sh
+++ generate-version.sh
@@ -2,18 +2,27 @@
 set -euo pipefail
 
 SHA=$(git rev-parse HEAD)
-BASE_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "dev")
+
+# Try to get last tag
+if BASE_TAG=$(git describe --tags --abbrev=0 2>/dev/null); then
+    COMMITS_SINCE_TAG=$(git rev-list "${BASE_TAG}..HEAD" --count)
+    echo "Found base tag: ${BASE_TAG} with ${COMMITS_SINCE_TAG} commits since"
+else
+    BASE_TAG="dev"
+    COMMITS_SINCE_TAG=$(git rev-list HEAD --count)
+    echo "Found base tag: ${BASE_TAG} with ${COMMITS_SINCE_TAG} commits since"
+fi
 
 # Determine tag state
 if ! git diff --quiet || ! git diff --cached --quiet; then
-    # Working tree or index is dirty
     TAG="${BASE_TAG}-dirty"
-elif [ "$(git rev-list "${BASE_TAG}..HEAD" --count)" -gt 0 ]; then
-    # Clean tree but ahead of the last tag
-    TAG="${BASE_TAG}-next"
+    echo "Working tree is dirty"
+elif [ "$COMMITS_SINCE_TAG" -gt 0 ]; then
+    TAG="${BASE_TAG}-$COMMITS_SINCE_TAG"
+    echo "There are $COMMITS_SINCE_TAG commits since the last tag"
 else
-    # Clean, exactly at the tag
     TAG="${BASE_TAG}"
+    echo "At exact tag ${BASE_TAG}"
 fi
 
 cat > version.libsonnet <<EOF
@@ -22,3 +31,6 @@ cat > version.libsonnet <<EOF
   tag: '${TAG}',
 }
 EOF
+
+echo "Generated version.libsonnet with sha ${SHA} and tag ${TAG}"
+
